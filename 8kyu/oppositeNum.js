4b9bf9cb6babd96ec0fd127c622d325bb93b5795
cerpinconsafo <pskolsky74@gmail.com>
2023-09-11 19:27:58 -0400

    oppositeNum

---
+++ 8kyu/oppositeNum.js
@@ -0,0 +1,33 @@
+
+// 8 kyu
+// Opposite number
+// 102330184% of 20,336116,689 of 284,908sergioet1 Issue Reported
+
+//     JavaScript
+//     Node v18.x
+
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+// Fundamentals
+
+
+// **********************************//
+//**********solution*************** *//
+
+function opposite(number) {
+    return number * (-1)
+  }
+
+  // or
+
+  const opposite = number => number * -1
+
+  // solution
+
+  // https://www.codewars.com/kata/56dec885c54a926dcd001095/solutions/javascript
