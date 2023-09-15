c40f01cb539682308c07671156fc5c548161f65c
cerpinconsafo <pskolsky74@gmail.com>
2023-09-15 12:52:25 -0400

    cwd2023 - string repeat

---
+++ 8kyu/stringRepeat.js
@@ -0,0 +1,27 @@
+// 8 kyu
+// String repeat
+
+//     JavaScript
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+// Examples (input -> output)
+
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+// Fundamentals
+// Strings
+
+// **********************************//
+//**********solution*************** *//
+
+function repeatStr (n, s) {
+    let string = '';
+    for (let i = 0; i < n; i++){ 
+      string+= s;}
+    return string
+  }
+
+ // solution page
+
+// https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/solutions/javascript
