217dc06debd73b3463d9ae622ca383cb8c056227
p.skol <pskolsky74@gmail.com>
2023-09-24 07:07:50 -0400

    cw2023 - under pressure, glamp upload

---
+++ 8kyu/underPressure1.js
@@ -0,0 +1,19 @@
+
+// 8 kyu
+// You Can't Code Under Pressure #1
+
+//     JavaScript
+
+// Code as fast as you can! You need to double the integer and return it.
+// Fundamentals
+
+// **********************************//
+//**********solution*************** *//
+
+function doubleInteger(i) {
+    return i = (i * 2);
+  }
+
+ // solution page
+
+// https://www.codewars.com/kata/53ee5429ba190077850011d4/solutions/javascript
