8518ef35b17d2e5cd6ba3e99ce5d4b70568dc230
p.skol <pskolsky74@gmail.com>
2023-10-06 16:47:33 -0400

    cw23 - simpleMultiplication and oppositesAttract

---
+++ 8kyu/simpleMultiplication.js
@@ -0,0 +1,22 @@
+// 8 kyu
+// Simple multiplication
+
+//     JavaScript
+
+// Instructions
+
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+// Fundamentals
+
+// **********************************//
+//**********solution*************** *//
+
+function simpleMultiplication(number) {
+    return number % 2 === 0 ? number * 8 : number * 9 
+}
+
+
+ // solution page
+
+// https://www.codewars.com/kata/583710ccaa6717322c000105/solutions/javascript
