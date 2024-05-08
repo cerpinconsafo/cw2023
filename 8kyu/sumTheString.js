8dad6fec3854450f8863a6ed7fefa025eb8bb80e
p.skol <pskolsky74@gmail.com>
2024-05-08 16:18:43 -0400

    daily cw 8kyu sum the strings

---
+++ 8kyu/sumTheString.js
@@ -0,0 +1,37 @@
+// 8 kyu
+// Sum The Strings
+// JavaScript
+
+// Description:
+
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+// Example: (Input1, Input2 -->Output)
+
+// "4",  "5" --> "9"
+// "34", "5" --> "39"
+// "", "" --> "0"
+// "2", "" --> "2"
+// "-5", "3" --> "-2"
+
+// Notes:
+
+//     If either input is an empty string, consider it as zero.
+
+//     Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+
+// Fundamentals
+
+// **********************************//
+//**********solution*************** *//
+
+function sumStr(a, b) {
+  //convert the entire expression to a String
+  return String(
+    //convert each parameter to an integer
+    Number(a) + Number(b)
+  );
+}
+// solution page
+
+//https://www.codewars.com/kata/5966e33c4e686b508700002d/solutions/javascript
