95d779b0cf0713be7feea610f22b694ccb8e3927
p.skol <pskolsky74@gmail.com>
2024-05-08 15:50:55 -0400

    daily cw 8kyu difference in age from youngest to oldest

---
+++ 8kyu/findDifferenceInAge.js
@@ -0,0 +1,38 @@
+// 8 kyu
+// Find the Difference in Age between Oldest and Youngest Family Members
+// 1245393% of 2,1579,076 of 16,260KaraM
+
+//     JavaScript
+//     Node v18.x
+
+//         VIM
+//         EMACS
+
+// Instructions
+// Output
+
+// At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
+
+// You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array (a tuple in Python) with [youngest age, oldest age, difference between the youngest and oldest age].
+// Algorithms
+// Arrays
+// **********************************//
+//**********solution*************** *//
+
+function differenceInAges(ages) {
+  //find min max?
+  let min = Math.min(...ages);
+  let max = Math.max(...ages);
+
+  //creat array for desired return values
+  let ageArr = [];
+
+  //push values to array
+  ageArr.push(min, max, max - min);
+
+  return ageArr;
+}
+
+// solution page
+
+// https://www.codewars.com/kata/5720a1cb65a504fdff0003e2/solutions/javascript
--- 8kyu/square(n)Sum.js
+++ 8kyu/square(n)Sum.js
@@ -1,19 +1,12 @@
 // 8 kyu
 // Square(n) Sum
-// 151838092% of 17,42592,370 of 256,299jhoffner1 Issue Reported
-
-//     JavaScript
-//     Node v18.x
-
-//         VIM
-//         EMACS
 
 // Instructions
 // Output
 
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
-// For example, for [1, 2, 2] it should return 9 because 12+22+22=91^2 + 2^2 + 2^2 = 912+22+22=9.
+// For example, for [1, 2, 2] it should return 9 because 1^2+2^2+2^2=9.
 // Arrays
 // Lists
 // Fundamentals
